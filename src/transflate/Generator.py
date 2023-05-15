5a77c742f90cffc17504eea0ad1eafa67a63a88b
Nikita Kapchenko <kapchenko.nikita@gmail.com>
2023-05-15 08:30:45 -0700

    end down-break all

--- src/transflate/Generator.py
+++ src/transflate/Generator.py
@@ -6,6 +6,7 @@ class Generator(nn.Module):
 
     def __init__(self, d_model, vocab_len):
         super().__init__()
+        # project output tensor to Target Vocab Probabilties
         self.proj = nn.Linear(d_model, vocab_len)
 
     def forward(self, x):
